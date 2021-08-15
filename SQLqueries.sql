11780792105df082257d63f0cd1850c38b725776
Kamesh V R <67401750+kamesh15@users.noreply.github.com>
2021-08-15 16:12:24 +0530

    Add files via upload

---
+++ SQLqueries.sql
@@ -0,0 +1,11 @@
+create database HMS;
+
+use ERS;
+
+create table EmployeeDetails(Employee_id varchar(20) primary key,Password varchar(20)  not null unique,First_Name varchar(20),Last_Name varchar(20),Designation varchar(20),Department varchar(20),Email_ID varchar (20) unique,Phone_No varchar(20) ,Type varchar(20));
+
+select * from EmployeeDetails;
+
+create table ReimbursementDetails(Reimbursement_Id integer AUTO_INCREMENT PRIMARY KEY, Employee_id varchar(20) ,Reimbursement_Type varchar(50),Reimbursement_Amount double, Description varchar(100),Invoice_Documents longblob,Reimbursement_Status varchar(20), foreign key(Employee_id) references EmployeeDetails(Employee_id) ); 
+
+select * from ReimbursementDetails;
